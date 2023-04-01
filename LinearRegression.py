ab4d86cda3af6d6c5f295fcbb38465594daceef3
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2023-04-01 18:15:39 +0500

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,27 +1,29 @@
 import torch
 from torch.autograd import Variable
 
-class linearRegression(torch.nn.Module):
+class linearRegression(torch.nn.Module):            
+    
+    # instantiating Linear Regression class
     def __init__(self):
         super(linearRegression, self).__init__() 
-        self.lr_pytorch = torch.nn.Linear(1,1)
+        self.lr_pytorch = torch.nn.Linear(1,1)                                                       # Linear block in pytorch
 
-    def forward(self,input_to_predict):
+    def forward(self,input_to_predict):                                                              # Forward propagation (Prediction)
         predictions = self.lr_pytorch(input_to_predict)
         return predictions
             
-    def fit(self,inputs,outputs,epochs = 500, learning_rate = 0.01):
-        #inputs = Variable(torch.Tensor(inputs))
-        #labels = Variable(torch.Tensor(outputs))
+    def fit(self,inputs,outputs,epochs = 500, learning_rate = 0.01):                                 # Fit method to train on existing data
         
-        stochastic_gradient_descent = torch.optim.SGD(lr_pytorch_model.parameters(),learning_rate)
-        mse_loss_function = torch.nn.MSELoss() 
+        inputs = Variable(torch.from_numpy(np.array(inputs,dtype=np.float32).reshape(-1,1)))        # converting np.array to tensors for operations
+        labels = Variable(torch.from_numpy(np.array(outputs,dtype=np.float32).reshape(-1,1))) 
         
-        for epoch in range(epochs):
+        stochastic_gradient_descent = torch.optim.SGD(lr_pytorch_model.parameters(),learning_rate)  # instantiating SGD Gradient Descent 
+        mse_loss_function = torch.nn.MSELoss()                                                      # Instantiationg MSE Loss function
+        
+        for epoch in range(epochs):                                                                 # Training 
             stochastic_gradient_descent.zero_grad()
             predictions = lr_pytorch_model(inputs)
             
             loss = mse_loss_function(predictions,labels)
             loss.backward()
             stochastic_gradient_descent.step()
-        
